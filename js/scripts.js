a4a7793b21cb213f881ee63f6bbb46ddaf7a2fd3
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-04-21 20:04:40 +0600

    Complete navbar responsiveness.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,28 @@
-(function($){
-	$(document).ready(function() {	
+(function ($) {
+    $(document).ready(function () {
+        // Scroll to Top
+        jQuery(".scrolltotop").click(function () {
+            jQuery("html").animate({ scrollTop: "0px" }, 400);
+            return false;
+        });
 
-		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
-			return false;
-		});
-		
-		jQuery(window).scroll(function(){
-			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
-				jQuery('.scrolltotop').fadeIn();
-			} else {
-				jQuery('.scrolltotop').fadeOut();
-			}
-		});
+        jQuery(window).scroll(function () {
+            var upto = jQuery(window).scrollTop();
+            if (upto > 500) {
+                jQuery(".scrolltotop").fadeIn();
+            } else {
+                jQuery(".scrolltotop").fadeOut();
+            }
+        });
 
-
-		
-
-				
-		
-		
-		
-		
-		
-		
-		
-		
-	});
+        // Mobile Menu Active
+        jQuery("#humbarger-icon").click(function () {
+            jQuery(".mobile-menu-wrpper").toggleClass("menu-active");
+        });
+        jQuery(window).on("resize", function () {
+            if (jQuery(window).width() > 992) {
+                jQuery(".mobile-menu-wrpper").removeClass("menu-active");
+            }
+        });
+    });
 })(jQuery);
